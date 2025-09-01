788c912b456f3c1b02e801bd114e7f0394e187d4
Don Kavindu Hansana Colombage <148005520+kavinduhcolombage@users.noreply.github.com>
2025-09-02 02:06:02 +0530

    Fix import case for AboutMe section

--- src/Content/Portfolio.tsx
+++ src/Content/Portfolio.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../Components/Navbar";
 import StarNetworkBackground from "../Components/StarNetworkBackground";
-import AboutMe from "../Sections/Aboutme";
+import AboutMe from "../Sections/AboutMe";
 import Education from "../Sections/Education";
 import Introduction from "../Sections/Introduction";
 import Project from "../Sections/Project";
