52a63e95a632b3a1fb5cbcfdc5749d8656ebafff
Kavindu Hansana <hansana2024@gmail.com>
2025-11-12 15:49:55 +0530

    implement ShinyText component for animated text display in Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,19 @@
+import ShinyText from "./ShinyText";
+
 const Navbar = () => {
   return (
     <nav className="border-b-4 border-transparent fixed w-full z-20">
       <div className="flex items-center justify-between py-8 px-6 mx-8 max-[400px]:mx-1">
         <a href="#">
-          <h1 className="text-3xl max-[900px]:text-2xl font-bold text-white bg-clip-text">
+          {/* <h1 className="text-3xl max-[900px]:text-2xl font-bold text-white bg-clip-text">
             Kavindu's Portfolio
-          </h1>
+          </h1> */}
+          <ShinyText
+            text="Kavindu's Portfolio"
+            disabled={false}
+            speed={3}
+            className='custom-class text-3xl max-[900px]:text-2xl font-bold'
+          />
         </a>
 
         <ul className="flex space-x-8 max-[700px]:hidden [&>*]:text-lg max-[900px]:[&>*]:text-base [&>*]:text-white ">
